f4847eaebe1adb56b14101027e4652a0a7c92121
abhaypendawle <pendawleabhay@gmail.com>
2016-05-01 16:57:24 -0700

    delivery time changed (*1000)

--- services/trips/tripshandler.js
+++ services/trips/tripshandler.js
@@ -299,7 +299,7 @@ _constructTripDetails = function (customer, farmer, product, journeyDetails, dri
 		customerAddress = customer.address + " , " + customer.city + " , " + customer.state + " , " + customer.zipCode,
 		farmerAddress = farmer.address + " , " + farmer.city + " , " + farmer.state + " , " + farmer.zipCode,
 		orderTime = new Date().getTime(),
-		deliveryTime = orderTime + (journeyDetails.timeRequired * 100),
+		deliveryTime = orderTime + (journeyDetails.timeRequired * 1000),
 		deliverySteps = journeyDetails.steps;
 
 	var tripDetails = {
